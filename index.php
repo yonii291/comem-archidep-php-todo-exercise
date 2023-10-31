9408adc2b22286d0a993ed3826e5e380f0ad8109
Yonatan Teklezgi <yonatan.teklezgi@heig-vd.ch>
2023-10-31 17:02:40 +0100

    Config var environnement user

--- index.php
+++ index.php
@@ -9,11 +9,11 @@
 define('BASE_URL', getenv('TODOLIST_BASE_URL') ?: '/');
 
 // Database connection parameters.
-define('DB_USER', 'todolist');
+define('DB_USER', getenv('TODOLIST_DB_USER') ?: 'todolist');
 define('DB_PASS', getenv('TODOLIST_DB_PASS'));
 define('DB_NAME', 'todolist');
 define('DB_HOST', '127.0.0.1');
-define('DB_PORT', '3306'); ///8889
+define('DB_PORT', '3306'); 
 
 try {
   $db = new PDO('mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
